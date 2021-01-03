4e653c4249fb3007fea03ba671874e17893a0482
Reza <hungry.dev39@gmail.com>
2021-01-03 20:30:08 +0700

    created eslint config & change server to use typescript

--- apps/server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-const PORT = 8000;
-
-app.get('/', (rq, rs) => rs.send('Hello World'));
-
-app.listen(PORT, () => console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`))
---
+++ apps/server/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (rq, rs) => rs.send('Hello World'));
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
