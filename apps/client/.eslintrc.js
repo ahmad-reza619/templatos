9cdd6f93ffb3499fcf3deb1fd8390150c62999e4
Reza <hungry.dev39@gmail.com>
2021-01-05 22:32:58 +0700

    add & setup eslint for client

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -13,14 +13,14 @@ module.exports = {
   settings: {
     "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
     "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
     "import/resolver": {
-      "typescript": {
-        "project": "./tsconfig.json"
+      typescript: {
+        project: "./tsconfig.json",
       },
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   },
@@ -33,12 +33,12 @@ module.exports = {
       "error",
       "ignorePackages",
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-   ],
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
     // note you must disable the base rule as it can report incorrect errors
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": ["error"],
